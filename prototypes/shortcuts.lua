6dfdf6d26a18e1987e77186ad825d4b23a1306e3
Tom Burrows <tomburrows13@icloud.com>
2022-04-02 14:02:36 +0100

    Add search icon to shortcut

--- prototypes/shortcuts.lua
+++ prototypes/shortcuts.lua
@@ -7,26 +7,26 @@ local search_shortcut = {
   order = "a",
   icon =
   {
-    filename = "__SpidertronEnhancements__/graphics/follow-shortcut.png",
+    filename = "__core__/graphics/icons/search-black.png",
     size = 32,
     flags = {"gui-icon"}
   },
-  small_icon = {
+  --[[small_icon = {
     filename = "__SpidertronEnhancements__/graphics/follow-shortcut-24.png",
     size = 24,
     flags = {"gui-icon"}
-  },
+  },]]
   disabled_icon = {
-    filename = "__SpidertronEnhancements__/graphics/follow-shortcut-white.png",
+    filename = "__core__/graphics/icons/search-white.png",
     size = 32,
     flags = {"gui-icon"}
   },
-  disabled_small_icon =
+  --[[disabled_small_icon =
   {
     filename = "__SpidertronEnhancements__/graphics/follow-shortcut-white-24.png",
     size = 24,
     flags = {"gui-icon"}
-  }
+  }]]
 }
 local search_input = {
 	type = "custom-input",
