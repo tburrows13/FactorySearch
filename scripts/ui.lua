6bc929378e902f78a2116f81de0aa8548c78861c
Tom Burrows <tomburrows13@icloud.com>
2022-04-03 01:17:17 +0100

    Change to search signals instead of items, to include fluids https://forums.factorio.com/viewtopic.php?p=541360#p541360

--- scripts/ui.lua
+++ scripts/ui.lua
@@ -139,12 +139,12 @@ local function build_gui(player)
                     {
                       type = "choose-elem-button",
                       style = "slot_button_in_shallow_frame",
-                      elem_type = "item",
+                      elem_type = "signal",
                       mouse_button_filter = {"left"},
                       ref = { "item_select" },
                       style_mods = {
-                        minimal_width = 60,
-                        minimal_height = 60,
+                        minimal_width = 50,
+                        minimal_height = 50,
                       },
                       actions = {
                         on_elem_changed = { gui = "search", action = "item_selected" }
@@ -244,7 +244,7 @@ event.on_gui_elem_changed(
           --  local surface = game.get_surface(surface_index)
             --local recipe_data = surface_data[item]
             -- TODO
-          local data = find_machines(item, force.name)
+          local data = find_machines(item.name, force.name)
           player_data.refs.result_flow.clear()
           build_result_gui(data, player_data.refs.result_flow)
         end
