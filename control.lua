8434ed243cd6c0e5c0a679e6f2a7b8356d02147c
Tom Burrows <tomburrows13@icloud.com>
2022-06-02 03:34:32 +0100

    Rename ui.lua -> gui.lua

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
 util = require "__core__.lualib.util"
 event = require "scripts.event"
 search = require "scripts.search"
-local ui = require "scripts.ui"
+local gui = require "scripts.gui"
 
 
 script.on_init(
@@ -16,7 +16,7 @@ script.on_configuration_changed(
     for player_index, player_data in pairs(global.players) do
       local player = game.get_player(player_index)
       if player then
-        ui.destroy_gui(player, player_data)
+        gui.destroy_gui(player, player_data)
       else
         global.players[player_index] = nil
       end
