5bc1babdf86a4fb63e8bcd3e086e915b8777ffdd
Tom Burrows <tomburrows13@icloud.com>
2022-12-14 01:48:28 +0000

    Make zoom level consistent across different screen resolutions

--- settings.lua
+++ settings.lua
@@ -20,7 +20,7 @@ data:extend{
     type = "double-setting",
     name = "fs-initial-zoom",
     setting_type = "runtime-per-user",
-    default_value = 0.6,
+    default_value = 0.5,
     minimum_value = 0.05,
     maximum_value = 10,
     order = "a",
