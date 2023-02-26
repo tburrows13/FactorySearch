f4db087efd1b9fbee5787fafd688aa599594f38b
Tom Burrows <tomburrows13@icloud.com>
2023-02-26 21:21:11 +0000

    Only create corpse signal if corpse has icon defined

--- data-updates.lua
+++ data-updates.lua
@@ -8,17 +8,19 @@ data:extend{
 }
 
 for name, corpse in pairs(data.raw["character-corpse"]) do
-  data:extend{
-    {
-      type = "virtual-signal",
-      name = name,
-      localised_name = corpse.localised_name or {"entity-name." .. name},
-      localised_description = corpse.localised_description or {"entity-description." .. name},
-      icon = corpse.icon,
-      icon_size = corpse.icon_size,
-      icon_mipmaps = corpse.icon_mipmaps,
-      icons = corpse.icons,
-      subgroup = "virtual-signal-corpse",
+  if corpse.icon or corpse.icons then
+    data:extend{
+      {
+        type = "virtual-signal",
+        name = name,
+        localised_name = corpse.localised_name or {"entity-name." .. name},
+        localised_description = corpse.localised_description or {"entity-description." .. name},
+        icon = corpse.icon,
+        icon_size = corpse.icon_size,
+        icon_mipmaps = corpse.icon_mipmaps,
+        icons = corpse.icons,
+        subgroup = "virtual-signal-corpse",
+      }
     }
-  }
+  end
 end
