c8d60cd933bf5beb1a951fdbe761c17f4e356d8c
Tom Burrows <tomburrows13@icloud.com>
2023-09-08 23:35:58 +0100

    Capitalise first letter of surface name

--- scripts/gui.lua
+++ scripts/gui.lua
@@ -105,10 +105,8 @@ end
 
 function Gui.build_surface_name(include_surface_name, surface_name)
   if include_surface_name then
-    if surface_name == "nauvis" then
-      -- Space Exploration capitilises all other planet names, so do Nauvis for consistency
-      surface_name = "Nauvis"
-    end
+    -- Capitalize first letter
+    surface_name = surface_name:gsub("^%l", string.upper)
     return  {
       type = "label",
       caption = surface_name,
