d6384a42f1e8670be5701ada9d9f4a709555066a
Tom Burrows <tomburrows13@icloud.com>
2025-04-01 11:31:22 +0100

    Fix type warnings for flib-gui style_mods

--- prototypes/custom-input.lua
+++ prototypes/custom-input.lua
@@ -1,7 +1,7 @@
 local open_search_prototype = {
-	type = "custom-input",
-	name = "open-search-prototype",
-	key_sequence = "SHIFT + ALT + mouse-button-1",
+  type = "custom-input",
+  name = "open-search-prototype",
+  key_sequence = "SHIFT + ALT + mouse-button-1",
   consuming = "none",
   include_selected_prototype = true,
   order = "a"
